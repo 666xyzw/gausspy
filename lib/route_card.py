35af6a3dbd8aa16bb75357a899885caaaa8cb176
666xyzw <51948554+666xyzw@users.noreply.github.com>
2019-08-08 23:02:02 +0300

    Update route_card.py

--- lib/route_card.py
+++ lib/route_card.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+#Written by xyz666
+
+"""
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.
+"""
+
+
+
 import os, re
 
 def route_dict():
