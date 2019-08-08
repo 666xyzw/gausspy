7fe58cb96f2dfb89432799a2687e08f75a98c26c
666xyzw <51948554+666xyzw@users.noreply.github.com>
2019-08-08 22:59:13 +0300

    Update cleaner.py

--- lib/cleaner.py
+++ lib/cleaner.py
@@ -1,5 +1,18 @@
 import os
 
+#Written by xyz666
+
+"""
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.
+"""
+
+
 def cleaner(answer):
 	"""
 	This function needs a parameter. If the parameter is no, then, in the imag folder, deletes everything (*.logs, *.xyz, *.xyz- and ginp) except the new input files.
