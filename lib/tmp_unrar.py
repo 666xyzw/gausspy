50e84a87e5d593bc9c6d58e5c1501ac397cfafac
666xyzw <jakoszabi@gmail.com>
2019-09-02 10:53:15 +0300

    new update

--- lib/tmp_unrar.py
+++ lib/tmp_unrar.py
@@ -13,6 +13,7 @@ but WITHOUT ANY WARRANTY.
 
 import os
 
+import threading
 
 def tmp_check(directory):
 	"""
@@ -31,26 +32,13 @@ def unrar(directory):
 	for f in os.listdir(directory):
 		try:
 			f_name, f_ext=f.split('.')
-			"""
-			if  f_ext=='tgz':
-				#print('Decompressing {}'.format(f))
+		
+			if 'error' not in f_name and f_ext=='tgz':	
+				
 				os.system('tar xzpf '+ f +' -C tmp ')
-			else:
-				#print('Decompressing {}'.format(f))
-				os.system('tar xpf '+ f +' -C tmp ')
-			"""
-			if 'error' in f_name and f_ext=='tgz':
-				pass
-
-			elif 'error' not in f_name and f_ext=='tgz':	
-				#print('Decompressing {}'.format(f))
-				os.system('tar xzpf '+ f +' -C tmp ')
-
-			elif 'error' in f_name and f_ext=='tar':
-				pass
 
 			elif 'error' not in f_name and f_ext=='tar':
-				#print('Decompressing {}'.format(f))
+	
 				os.system('tar xpf '+ f +' -C tmp ')
 
 			else:
@@ -60,4 +48,3 @@ def unrar(directory):
 			pass
 	
 	os.chdir('tmp/')
-
